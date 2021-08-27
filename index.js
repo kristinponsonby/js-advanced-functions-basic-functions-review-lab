a7b50c7c3eccae0da735fb0a289c3ef041ffb895
Kristin <kristinponsonby@gmail.com>
2021-08-27 15:13:12 -0400

    commit tree 7f0c71dd3335cd4053047f254d03590dd3045b48

--- index.js
+++ index.js
@@ -7,7 +7,41 @@ function mondayWork(activity="go to the office") {
     return `This Monday, I will ${activity}.`
 }
 
-function wrapAdjective() {
-    return function(param="special") {
-    
+function wrapAdjective(special="*") {
+    return function(string, param="special") {
+        return `You are ${special}${string}${special}!` 
+    }
 }
+
+function wrapAdjective(special="*") {
+    return function(string, param="special") {
+        return `You are ${special}${string}${special}!` 
+    }
+}
+
+
+const Calculator = {
+    add: function(a,b) {
+      return a + b;
+    },
+    subtract: function(a,b) {
+      return a - b
+    },
+    multiply: function(a,b) {
+      return a * b
+    },
+    divide: function(a,b) {
+      return a / b;
+    }
+  }
+
+ 
+let actionApplyer = function(start, array) {
+    let a = start
+  
+    for (let i = 0; i < array.length; i++ ){
+      a = array[i](a)
+    }
+  
+    return a
+  }
