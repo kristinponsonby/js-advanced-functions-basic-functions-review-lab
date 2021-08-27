8ff4fa9585eec2f3c2a7ed943a8e7192737bb6d2
Kristin <kristinponsonby@gmail.com>
2021-08-27 14:25:22 -0400

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function saturdayFun(activity="roller-skate") {
     return `This Saturday, I want to ${activity}!`
 }
 saturdayFun()
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective() {
+    return function(param="special") {
+    
+}
